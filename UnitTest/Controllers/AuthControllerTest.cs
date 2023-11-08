da352c9ac96ed0fc2dcb83191291ad4e8c87b6a8
Kevin <kevin11062011@gmail.com>
2023-11-08 17:08:50 -0500

    unit tests

---
+++ UnitTest/Controllers/AuthControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TopTenMoviesAPI.Controllers;
+
+namespace UnitTest.Controllers
+{
+    public class AuthControllerTest
+    {
+        private AuthController _controller;
+        private IConfiguration _configuration;
+
+        public AuthControllerTest()
+        {
+            var inMemorySettings = new Dictionary<string, string>()
+            {
+                { "Jwt:Issuer", "kevinissuer" },
+                { "Jwt:Key", "218d12mt883gnb8238v" },
+            };
+
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+            _controller = new AuthController(_configuration);
+        }
+
+        [Fact]
+        public void Post_ShouldReturnOkWithValidToken()
+        {
+
+            // Act
+            var result = _controller.Post();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+
+            Assert.NotNull(okResult.Value);
+            var token = (string)okResult.Value;
+
+            // Validate the JWT token
+            var jwtSecurityHandler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = jwtSecurityHandler.ReadJwtToken(token);
+
+            Assert.NotNull(jwtSecurityToken);
+            Assert.Equal(_configuration["Jwt:Issuer"], jwtSecurityToken.Issuer);
+        }
+    }
+}
---
+++ UnitTest/Controllers/MoviesControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TopTenMoviesAPI.Controllers;
+using TopTenMoviesAPI.Model;
+using TopTenMoviesAPI.Services;
+
+namespace UnitTest.Controllers
+{
+    public class MoviesControllerTests
+    {
+        private MoviesController controller;
+        private readonly Mock<IMovieService> mockMovieService;
+
+        public MoviesControllerTests()
+        {
+            mockMovieService = new Mock<IMovieService>();
+        }
+
+
+        [Fact]
+        public async Task GetMovies_ShouldReturnOkWithValidMovies()
+        {
+            var expectedMovies = new List<Movie>()
+            {
+                new Movie { Title = "The Shawshank Redemption" },
+                new Movie { Title = "The Godfather" },
+            };
+            mockMovieService.Setup(m => m.GetMovies()).ReturnsAsync(expectedMovies);
+
+            // Act
+            controller = new MoviesController(mockMovieService.Object);
+            var result =  controller.GetMovies();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+
+            Assert.NotNull(okResult.Value);
+            var response = (ApiResponse<List<Movie>>)okResult.Value;
+
+            Assert.True(response.Success);
+        }
+
+        [Fact]
+        public async Task GetMovies_ShouldReturnInternalServerErrorWithUnexpectedError()
+        {
+            // Arrange
+            mockMovieService.Setup(m => m.GetMovies()).ThrowsAsync(new Exception("An unexpected error occurred."));
+
+            // Act
+            controller = new MoviesController(mockMovieService.Object);
+            var result = controller.GetMovies();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+
+            Assert.NotNull(okResult.Value);
+            var response = (ApiResponse<List<Movie>>)okResult.Value;
+
+            Assert.False(response.Success);
+        }
+
+        [Fact]
+        public async Task NotifyMoviesReceived()
+        {
+            var expectedMovies = new List<string>()
+            {
+                "The Shawshank Redemption",
+                "The Godfather"
+            };
+            mockMovieService.Setup(m => m.NotifyMoviesReceived("1001235297", expectedMovies));
+
+            // Act
+            controller = new MoviesController(mockMovieService.Object);
+            var result = controller.NotifyMoviesReceived(new Request() { RUT = "1001235297", Peliculas = expectedMovies });
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+
+            Assert.NotNull(okResult.Value);
+            var response = (ApiResponse<string>)okResult.Value;
+
+            Assert.True(response.Success);
+        }
+    }
+}
